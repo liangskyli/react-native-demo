8fdc2ed66ca482a3dddf58ef0e4b5b3ab2aff90b
liangsky <640634387@qq.com>
2025-11-01 15:51:42 +0800

    feat: webview props support

--- src/screens/webview/index.tsx
+++ src/screens/webview/index.tsx
@@ -1,11 +1,43 @@
+import AssetsImage from '@/components/assets-image';
+import Icon from '@/components/icon';
 import SafeScreen from '@/components/safe-screen';
 import type { Paths } from '@/navigation/paths.ts';
 import type { RootNativeStackScreenProps } from '@/navigation/types.ts';
-import { ActivityIndicator, View } from 'react-native';
+import { useLayoutEffect, useRef, useState } from 'react';
+import { ActivityIndicator, TouchableOpacity, View } from 'react-native';
 import WebView from 'react-native-webview';
 
 const WebViewScreen = (props: RootNativeStackScreenProps<Paths.webView>) => {
   const { route, navigation } = props;
+  const webViewRef = useRef<WebView>(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // eslint-disable-next-line react/no-unstable-nested-components
+      headerLeft: () => {
+        return (
+          <>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Icon path="close" className="size-[28px] color-[#007AFF]" />
+            </TouchableOpacity>
+            {canGoBack && (
+              <TouchableOpacity
+                onPress={() => {
+                  webViewRef.current!.goBack();
+                }}
+              >
+                <AssetsImage
+                  path="back-icon"
+                  className="ml-2 h-[21px] w-[12px]"
+                />
+              </TouchableOpacity>
+            )}
+          </>
+        );
+      },
+    });
+  }, [canGoBack, navigation]);
   return (
     <SafeScreen
       className="flex-1 items-center justify-center"
@@ -13,13 +45,16 @@ const WebViewScreen = (props: RootNativeStackScreenProps<Paths.webView>) => {
     >
       <View className="h-full w-full flex-1">
         <WebView
-          source={{ uri: route.params.uri }}
+          ref={webViewRef}
           renderLoading={() => (
             <ActivityIndicator className="absolute inset-0" />
           )}
           startInLoadingState={true}
+          {...route.params}
           onNavigationStateChange={navState => {
+            setCanGoBack(navState.canGoBack);
             navigation.setOptions({ title: navState.title });
+            route.params.onNavigationStateChange?.(navState);
           }}
         />
       </View>
